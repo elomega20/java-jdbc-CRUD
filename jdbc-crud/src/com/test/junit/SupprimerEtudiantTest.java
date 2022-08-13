884a8ebdcd5540e3f10f9ab8db11d4818176efe1
el sedy ba <elsedy000@gmail.com>
2022-08-13 15:56:32 +0000

    test unitaire de la fonctionnalite supprimerEtudiant

---
+++ jdbc-crud/src/com/test/junit/SupprimerEtudiantTest.java
@@ -0,0 +1,15 @@
+package com.test.junit;
+
+import org.junit.jupiter.api.Test;
+
+import com.crud.Etudiant;
+
+class SupprimerEtudiantTest {
+
+	@Test
+	void supprimeretudiantTest() {
+		Etudiant etudiant = new Etudiant("p25 118", "jemy","123","Bond","James", "jemy@gmail.com");
+		etudiant.supprimerEtudiant();
+	}
+
+}
