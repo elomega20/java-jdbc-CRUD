7927ff0c501f9cf1f0ba80ea94f32db15b7caade
el sedy ba <elsedy000@gmail.com>
2022-08-13 14:16:34 +0000

    test unitaire de insererEtudiant

---
+++ jdbc-crud/src/com/test/junit/InsererEtudiantTest.java
@@ -0,0 +1,15 @@
+package com.test.junit;
+
+import org.junit.jupiter.api.Test;
+
+import com.crud.Etudiant;
+
+class InsererEtudiantTest {
+
+	@Test
+	void test() {
+		Etudiant etudiant = new Etudiant("p26 118", "jemy","123","Bond","James", "jemy@gmail.com");
+		etudiant.insererEtudiant();
+	}
+
+}
