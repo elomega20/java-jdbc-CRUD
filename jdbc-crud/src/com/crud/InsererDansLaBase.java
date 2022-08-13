b8e5831bf680fd6f69d8b8486b2b5d055fa377b6
el sedy ba <elsedy000@gmail.com>
2022-08-13 13:25:35 +0000

    fonctionnalite insererEtudiant et etablissement du test avec Junit

--- jdbc-crud/src/com/crud/InsererDansLaBase.java
+++ jdbc-crud/src/com/crud/InsererDansLaBase.java
@@ -2,19 +2,30 @@ package com.crud;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import com.connexion.Connecter;
 
 public class InsererDansLaBase {
 
 	public static void insererEtudiant(String codeEtudiant,String login,String passworld,String nom,String prenom,String email) {
-		
+		//on recupere l'url , le login et le passworld
 		String[] parametreDeConnexion = new String[3];
 		parametreDeConnexion = Connecter.parametreConnexion();
 		
-		try(Connection connexion = DriverManager.getConnection(parametreDeConnexion[0], parametreDeConnexion[1], parametreDeConnexion[2])){
-			System.out.println("Connection etablie avec succes");
+		String requeteSql = "INSERT INTO etudiant VALUES(?,?,?,?,?,?)";
+		//etablissement de la coonnexion et execution de la requete
+		try(Connection connexion = DriverManager.getConnection(parametreDeConnexion[0], parametreDeConnexion[1], parametreDeConnexion[2]);
+				PreparedStatement preparedStatement = connexion.prepareStatement(requeteSql)){
+			
+		    preparedStatement.setString(1, codeEtudiant);
+		    preparedStatement.setString(2, login);
+		    preparedStatement.setString(3, passworld);
+		    preparedStatement.setString(4, nom);
+		    preparedStatement.setString(5, prenom);
+		    preparedStatement.setString(6, email);
+		    
+		    preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- jdbc-crud/src/com/test/junit/TestInsererDansLaBase.java
+++ jdbc-crud/src/com/test/junit/TestInsererDansLaBase.java
@@ -1,6 +1,5 @@
 package com.test.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import com.crud.InsererDansLaBase;
 
