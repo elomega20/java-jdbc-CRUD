1a9d0906dc438f89e3f02ba423483f91a2078dda
el sedy ba <elsedy000@gmail.com>
2022-08-14 20:55:22 +0000

    creation de quelques  etudiant et module

--- jdbc-crud/src/com/elomega/CrudJdbc.java
+++ jdbc-crud/src/com/elomega/CrudJdbc.java
@@ -1,10 +1,32 @@
 package com.elomega;
 
+import com.crud.Etudiant;
+import com.crud.Module;
+
 public class CrudJdbc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// on cree des etudiants
+		Etudiant etudiantEl = new Etudiant("p23  14", "elzo", "123elzo", "el hadji", "sow", "elzo@gmail.com");
+		Etudiant etudiantMoussa = new Etudiant("p23  15", "mss", "123mss", "moussa", "diop", "mss@gmail.com");
+		Etudiant etudiantLala = new Etudiant("p23  16", "ll", "123ll", "lala", "ba", "ll@gmail.com");
+		
+		// on cree des modules
+		Module moduleAlgebre = new Module("UE237", "Algebre", 5);
+		Module moduleAnalyse = new Module("UE238", "Analyse", 5);
+		Module moduleCripto  = new Module("UE239", "Criptographie", 5);
+		Module moduleJava    = new Module("UE240","Java",6);
+
+		// on insere les etudiants dans la base
+		etudiantEl.insererEtudiant();
+		etudiantMoussa.insererEtudiant();
+		etudiantLala.insererEtudiant();
+
+		// on insere les modules dans la base
+		moduleAlgebre.insererModule();
+		moduleAnalyse.insererModule();
+		moduleCripto.insererModule();
 	}
 
 }
